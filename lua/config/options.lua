0000d1c537fe989c10e54f54b5ffef617f8484cf
David Majomi <majomidavid1@gmail.com>
2026-07-16 13:40:39 -0400

    Fix: Update root detection, remove unused plugins, and filter out ignored files.

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,8 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- lua/config/autocmds.lua or options.lua
+
+-- Overide default root detection to use only git and cwd rather than other files like Dockerfiles.
+vim.g.root_spec = { { ".git" }, "cwd" }
